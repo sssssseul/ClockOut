b2eaa6f3dd7997d549562ae5be820c0f7b792c3a
sssssseul <dgt3208@gmail.com>
2026-07-20 15:26:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -81,18 +81,6 @@ init_db()
 def index():
     return send_file('index.html')
 
-@app.route('/manifest.json')
-def manifest():
-    return send_file('manifest.json', mimetype='application/json')
-
-@app.route('/lotso.png')
-def lotso():
-    return send_file('lotso.png', mimetype='image/png')
-
-@app.route('/apple-touch-icon.png')
-def apple_touch_icon():
-    return send_file('apple-touch-icon.png', mimetype='image/png')
-
 @app.route('/api/nickname')
 def api_nickname():
     conn = get_db()
